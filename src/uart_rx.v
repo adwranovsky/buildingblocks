9b3a08c24a3085278f704ffb362916fa4fb7fd98
Alexander Wranovsky <alex@wranovsky.org>
2020-01-10 21:54:30 -0600

    Added first draft uart_rx module with empty testbench.

--- src/uart_rx.v
+++ src/uart_rx.v
@@ -19,37 +19,90 @@ module uart_rx(
     output valid
 );
 
-parameter CLK_FREQ   = 100_000_000;
-parameter BAUD       = 115200;
-parameter BIT_PERIOD = CLK_FREQ / BAUD;
+parameter CLK_FREQ        = 100_000_000;
+parameter BAUD            = 115200;
+parameter HALF_BIT_PERIOD = CLK_FREQ / BAUD / 2;
 
 reg [5:0] state, next_state;
+reg [7:0] data_out_reg, next_data_out_reg;
+reg [2:0] bit_counter, next_bit_counter;
+reg valid_reg;
+reg timer_start;
+wire timer_done;
+
+assign data_out = data_out_reg;
+assign valid = valid_reg;
+
+timer #(.STOP_COUNT(HALF_BIT_PERIOD)) timer(
+    .clk(clk),
+    .rst_n(rst_n),
+    .start(timer_start),
+    .done(timer_done)
+);
 
 always @(*) begin
-    next_state = `IDLE;
+    next_state = state;
+    next_data_out_reg = data_out_reg;
+    next_bit_counter = bit_counter;
+    valid_reg = 0;
+    timer_start = 1;
     case (state)
-        `IDLE: begin
+    `IDLE: begin
+        if (data_in == 1'b0) begin
+            // The start bit is beginning.
+            next_state = `START1;
+        end else begin
+            timer_start = 0;
         end
-        `START1: begin
+    end
+    `START1: begin
+        if (timer_done == 1'b1) begin
+            next_state = `START2;
         end
-        `START2: begin
+    end
+    `START2: begin
+        if (timer_done == 1'b1) begin
+            // Start bit is done, so move onto the first data bit.
+            next_state = `BIT_HALF1;
+            next_bit_counter = 0;
         end
-        `BIT_HALF1: begin
+    end
+    `BIT_HALF1: begin
+        if (timer_done == 1'b1) begin
+            // Sample the next bit.
+            next_state = `BIT_HALF2;
+            next_bit_counter = bit_counter + 1;
+            next_data_out_reg = {data_out_reg[7:1], data_in};
         end
-        `BIT_HALF2: begin
+    end
+    `BIT_HALF2: begin
+        if (timer_done == 1'b1) begin
+            if (bit_counter == 3'd7) begin
+                valid_reg = 1;
+                next_state = `STOP;
+            end else begin
+                next_state = `BIT_HALF2;
+            end
         end
-        `STOP: begin
+    end
+    `STOP: begin
+        if (timer_done == 1'b1) begin
+            next_state = `IDLE;
+            timer_start = 0;
         end
-        default: begin
-        end
+    end
     endcase
 end
 
 always @(posedge clk) begin
     if (rst_n == 0) begin
         state <= `IDLE;
+        data_out_reg <= 0;
+        bit_counter <= 0;
     end else begin
         state <= next_state;
+        data_out_reg <= next_data_out_reg;
+        bit_counter <= next_bit_counter;
     end
 end
 
