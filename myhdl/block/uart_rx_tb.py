de6579e2b67053dc65341192b5edcaa15daaefc9
Alexander Wranovsky <alex@wranovsky.org>
2020-01-18 18:12:50 -0600

    Created a testbench for the uart_rx module

---
+++ myhdl/block/uart_rx_tb.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+import sys
+import random
+
+from colorama import Fore, Back, Style
+from myhdl import *
+from uart_rx import *
+
+def uart_tx_model(tx, data, baud):
+    """Models a UART with 1 start bit, 8 data bits, and 1 stop bit.
+    tx -- serial data output
+    data -- the byte to send on tx
+    baud -- a baud object that represents the baud rate
+    """
+    print("-- Transmitting 0x%x" % data)
+    print("--- start bit")
+    tx.next = 0
+    yield delay(baud.bit_period)
+    for bit in range(8):
+        print("--- %d" % data[bit])
+        tx.next = data[bit]
+        yield delay(baud.bit_period)
+    print("--- stop bit")
+    tx.next = 1
+    yield delay(baud.bit_period)
+
+@block
+def uart_rx_tb():
+    """A testbench for the uart rx block"""
+    random.seed(0x0a310a31)
+
+    test_bytes = [random.randint(0, (1<<8)-1) for x in range(50)]
+
+    cycle_count = Signal(intbv(0))
+    error_count = Signal(intbv(0))
+
+    rst_n = ResetSignal(0, active=0, isasync=True)
+    clk = Signal(bool(0))
+
+    tx = Signal(intbv(1)[1:0])
+    data_read = Signal(intbv(0)[8:0])
+    valid = Signal(0)
+
+    baud = Baud(100e6, 1_000_000)
+    uart = uart_rx(clk=clk, reset=rst_n, serial_in=tx, byte_out=data_read, valid=valid, baud=baud)
+
+    start_tx = Signal(0)
+    tx_byte = Signal(intbv(0)[8:0])
+
+    @instance
+    def tx_generator():
+        while True:
+            yield start_tx.posedge
+            yield delay(1)
+            start_tx.next = 0
+            yield uart_tx_model(tx, tx_byte, baud)
+
+    @instance
+    def tests():
+        # Wait for a couple clock signals to deaasert reset
+        for x in range(5):
+            yield clk.negedge
+        rst_n.next = 1
+
+        # Wait a couple more negative clock edges
+        for x in range(5):
+            yield clk.negedge
+
+        # Test receiving each byte in the test vector
+        for byte in test_bytes:
+            tx_byte.next = byte
+            start_tx.next = 1
+            yield valid.posedge
+            if data_read == byte:
+                print(
+                    Fore.GREEN + "PASSED" + Style.RESET_ALL + " -- "
+                    + "byte read matched 0x%x" % byte
+                )
+            else:
+                print(
+                    Fore.RED + "FAILED" + Style.RESET_ALL + " -- "
+                    + "read 0x%x, expected 0x%x" % (data_read, byte)
+                )
+                error_count.next += 1
+
+        # Check errors and end simulation
+        yield delay(1)
+        if error_count == 0:
+            print(Fore.GREEN + "All tests passed :)" + Style.RESET_ALL)
+        else:
+            print(Fore.RED + "Failed %d tests"%error_count + Style.RESET_ALL)
+
+        raise StopSimulation
+
+    @always(clk.posedge)
+    def cycle_counter():
+        cycle_count.next += 1
+        if cycle_count > 20000:
+            print("Hit 20k cycles. Something's wrong.")
+            raise StopSimulation
+
+    @always(delay(10))
+    def clock_generator():
+        clk.next = not clk
+
+    return instances()
+
+def run(do_dump):
+    tb = uart_rx_tb()
+    tb.config_sim(trace=do_dump)
+    tb.run_sim()
+
+if __name__ == "__main__":
+    try:
+        if sys.argv[1] == "-d":
+            do_dump = True
+    except IndexError:
+        do_dump = False
+    run(do_dump)
