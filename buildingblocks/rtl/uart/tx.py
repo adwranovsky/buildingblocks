0a0dee45427cca192109360507ea52280cd86eb5
Alexander Wranovsky <alex@wranovsky.org>
2020-01-27 21:57:16 -0600

    Got the uart.tx module working

--- buildingblocks/rtl/uart/tx.py
+++ buildingblocks/rtl/uart/tx.py
@@ -19,7 +19,7 @@ def tx(clk, reset, serial_out, byte_in, start, done, baud):
     state = Signal(t_state.IDLE)
 
     # Holds the data that is being sent, including a start bit and a stop bit
-    shift_reg = Signal(intbv(0)[10:])
+    shift_reg = Signal(intbv(0x3ff)[10:])
 
     # The done signal of full_bit_timer indicates that the last bit was just sent
     full_bit_tstart = Signal(intbv(0)[1:])
@@ -27,28 +27,34 @@ def tx(clk, reset, serial_out, byte_in, start, done, baud):
     full_bit_timer = timer(clk=clk, reset=reset, start=full_bit_tstart, done=full_bit_done, STOP_COUNT=baud.clocks_per_bit)
 
     # Counts the number of bits sent
-    bit_count = Signal(intbv(0, min=0, max=9)) # 8 data bits + 1 stop bit = max count of 9
+    bit_count = Signal(intbv(0, min=0, max=10)) # 8 data bits + 1 start bit + 1 stop bit = max count of 10
     bit_counter = incrementer(clk=clk, reset=reset, inc=full_bit_done, clear=done, count=bit_count)
 
     @always_comb
+    def done_logic():
+        done.next = state==t_state.BUSY and bit_count == bit_count.max-1 and full_bit_done
+
+    @always_comb
     def fsm_comb_logic():
-        done.next = state==t_state.BUSY and bit_count == bit_count.max-1
-        full_bit_tstart.next = (state==t_state.IDLE and start) or (state==t_state.BUSY and full_bit_done and not done)
+        full_bit_tstart.next = (state==t_state.IDLE and start) or (state==t_state.BUSY and (full_bit_done and not done or start and done))
         serial_out.next = shift_reg[0]
 
-    @always_seq(clk, reset=reset)
+    @always_seq(clk.posedge, reset=reset)
     def fsm_seq_logic():
         state.next = state
         shift_reg.next = shift_reg
         if state == t_state.IDLE:
             if start:
                 state.next = t_state.BUSY
-                shift_reg.next = concat(1, byte_in[8:], 0)
+                shift_reg.next = concat('1', byte_in[8:], '0')
         elif state == t_state.BUSY:
             if full_bit_done:
-                shift_reg.next = concat(1, shift_reg[9:])
+                shift_reg.next = concat('1', shift_reg[10:1])
             if done:
-                state.next = t_state.IDLE
+                if start:
+                    shift_reg.next = concat('1', byte_in[8:], '0')
+                else:
+                    state.next = t_state.IDLE
         else:
             raise ValueError("Undefined state")
 
